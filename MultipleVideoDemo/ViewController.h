14f29a33b31823ea77cd4c825b1bc3f329d96eef
somdev choudhary <somdev@sparxitsolutions.com>
2016-09-01 12:50:22 +0530

    Initial Commit

---
+++ MultipleVideoDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MultipleVideoDemo
+//
+//  Created by SIS mobile on 9/1/16.
+//  Copyright © 2016 sparx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MultipleVideoDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MultipleVideoDemo
+//
+//  Created by SIS mobile on 9/1/16.
+//  Copyright © 2016 sparx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
